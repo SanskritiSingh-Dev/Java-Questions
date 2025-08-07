6f0682e281bfca6ff393be654595e152ae1eef35
Sanskriti Singh <singhsanskriti140804@gmail.com>
2025-08-07 10:45:56 +0530

    initial commit

---
+++ Arrays/binarySearch.java
@@ -0,0 +1,41 @@
+//binary search
+import java.util.*;
+
+public class binarySearch {
+    public static int binarySearch(int num[], int key){
+        // initializing the start and end index
+        int start = 0;
+        int end = num.length - 1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+            // if the key is found at mid index
+            if(num[mid] == key){
+                return mid; // return the index of the key
+            }
+            // if the key is less than the mid element, search in the left half
+            else if(num[mid] > key){
+                end = mid - 1;
+            }
+            // if the key is greater than the mid element, search in the right half
+            else{
+                start = mid + 1;
+            }
+
+        }
+        return -1; // return -1 if the key is not found
+        
+    }
+
+    public static void main(String[] args) {
+        int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 10;
+        int result = binarySearch(num, key);
+        if(result == -1){
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+    
+}
--- Arrays/binnarySearch.java
+++
@@ -1,9 +0,0 @@
-//binary search
-import java.util.*;
-
-public class binnarySearch {
-    public static int binarySearch(int num[], int key){
-        
-    }
-    
-}
