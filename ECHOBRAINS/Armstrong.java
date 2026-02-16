e2f4aa6ad3e75db33ee41209dff99edb4b92e57a
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-02-16 11:58:39 +0530

    Number problem practice

--- ECHOBRAINS/Armstrong.java
+++ ECHOBRAINS/Armstrong.java
@@ -2,32 +2,44 @@
 	An Armstrong number is a number where: Each digit is raised to the power of number of digits, The sum of those values equals the original number(153). */
 import java.util.Scanner;
 import java.lang.Math;
+
 public class Armstrong {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number: ");
-        int num = sc.nextInt();
-        int sum = 0;
-        int org = num;
-        int digitCount = 0;
-        while(num > 0){
-            digitCount++;
-            num = num/10;
-        }
-        num = org;
-        for(int i = 1; i <= digitCount; i++){
-            int digit = num % 10;
-            sum = sum + (int)Math.pow(digit, digitCount);
-            num =num /10;
-        }   
-        if(sum == org){
-            System.out.println("armstrong number");
-        }
-        else{
-            System.out.println("not an armstrong number");
-        }
-        sc.close();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range to check the armstrong number: ");
+        int range = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 1; i <= range; i++){
+
+            int num = i;
+            int sum = 0;
+            int org = num;
+            int digitCount = 0;
+
+            while(num > 0){
+                digitCount++;
+                num = num / 10;
+            }
+
+            num = org;
+
+            while(num > 0){
+                int digit = num % 10;
+                sum += (int)Math.pow(digit, digitCount);
+                num = num / 10;
+            }
+
+            if(sum == org){
+                count++;
+            }
+        }
+        System.out.println("Total Armstrong numbers are: " + count);
+
+        sc.close();
     }
-    
 }
+
+
